6d3a730fb92ac6e3937ce911fe316b1d92a6e283
DaWebAlpha <tribillio@gmail.com>
2025-06-15 09:42:35 -0700

    Added productroutes

--- routes/productroutes.js
+++ routes/productroutes.js
@@ -1,11 +1,24 @@
-import express from 'express'
-import { 
-    listProducts 
+import express from 'express';
+import { ensureAdmin, ensureUser } from '../middleware/authmiddleware.js';
+import {
+  listProducts,
+  listSingleProduct,
+  createProduct,
+  editProduct,
+  deleteProduct,
+  deleteAllProduct
 } from "../controllers/productControllers.js";
-import { getDataFromDb } from "../config/db1.js";
 
 const productRouter = express.Router();
 
-productRouter.get('/products', listProducts(getDataFromDb))
+productRouter.get('/admin/products', ensureAdmin(), listProducts);
+productRouter.get('/admin/products/:id', ensureAdmin(), listSingleProduct);
+productRouter.post('/admin/products', ensureAdmin(), createProduct());
+productRouter.put('/admin/products/:id', ensureAdmin(), editProduct());
+productRouter.delete('/admin/products/:id', ensureAdmin(), deleteProduct());
+productRouter.delete('/admin/products', ensureAdmin(), deleteAllProduct());
+
+productRouter.get('/products', ensureUser(), listProducts);
+productRouter.get('/products/:id', ensureUser(), listSingleProduct);
 
 export default productRouter;
